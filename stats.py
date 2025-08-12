8272affb29170b6f7d40cc77050af08a4531ec68
Shubha T (MS/EHM4-BST) <T.Shubha@in.bosch.com>
2025-08-12 15:31:43 +0530

    ERROR

--- stats.py
+++ stats.py
@@ -1,4 +1,34 @@
+import math
+import unittest
+import stats as statistics
 
-def calculateStats(numbers):
-  # implement the computation of statistics here and return the results
-  return None
+
+class StatsTest(unittest.TestCase):
+  def test_report_min_max_avg(self):
+    computedStats = statistics.calculateStats([1.5, 8.9, 3.2, 4.5])
+    epsilon = 0.001
+    self.assertAlmostEqual(computedStats["avg"], 4.525, delta=epsilon)
+    self.assertAlmostEqual(computedStats["max"], 8.9, delta=epsilon)
+    self.assertAlmostEqual(computedStats["min"], 1.5, delta=epsilon)
+
+  def test_avg_is_nan_for_empty_input(self):
+    computedStats = statistics.calculateStats([])
+    self.assertTrue(math.isnan(computedStats["avg"]))
+    self.assertTrue(math.isnan(computedStats["max"]))
+    self.assertTrue(math.isnan(computedStats["min"]))
+
+  def test_avg_ignore_nan_in_input(self):
+    computedStats = statistics.calculateStats([1.5, float('nan'), 3.2, 4.5])                              
+    epsilon = 0.001
+    self.assertAlmostEqual(computedStats["avg"], 3.067, delta=epsilon)
+    self.assertAlmostEqual(computedStats["max"], 4.5, delta=epsilon)
+    self.assertAlmostEqual(computedStats["min"], 1.5, delta=epsilon)
+
+  def test_avg_nan_for_all_nan_input(self):
+    computedStats = statistics.calculateStats([])
+    self.assertTrue(math.isnan(computedStats["avg"]))
+    self.assertTrue(math.isnan(computedStats["max"]))
+    self.assertTrue(math.isnan(computedStats["min"]))
+           
+if __name__ == "__main__":
+  unittest.main()
